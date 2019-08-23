831831133f02d553d6dcc0a7355bf96d53feac52
Mykhailo Aralov <mykhailo.aralov@softengi.com>
2019-08-23 17:13:53 +0300

    + script

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+
+
 const gulp            = require('gulp'),
       autoprefixer    = require('gulp-autoprefixer'),
       sass            = require('gulp-sass'),
@@ -14,7 +16,10 @@ const gulp            = require('gulp'),
 	    svgmin          = require('gulp-svgmin'),
 	    cheerio         = require('gulp-cheerio'),
       replace         = require('gulp-replace'),
-      tinypng         = require('gulp-tinypng');
+      tinypng         = require('gulp-tinypng'),
+      jshint          = require("gulp-jshint"),
+      babel           = require("gulp-babel"),
+      uglify          = require('gulp-uglify');
 
 
 const isDev = process.argv.indexOf('--dev') !== -1; //develop mode
@@ -25,8 +30,8 @@ const path = {
   src: {
     html:     'src/pages/*.html',
     sass:     'src/styles/*.scss',
-    js:       'src/js/*.js',
-    jsVen:    'src/js/**/*.js',
+    js:       'src/js/main.js',
+    jsVen:    'src/js/vendor/*.js',
     svg:      'src/media-files/img/*.svg',
     img:      'src/media-files/img/**/*.+(jpg|png)'
   },
@@ -42,7 +47,7 @@ const path = {
   watch: { 
     html:       'src/**/*.html',
     sass:       'src/**/*.scss',
-    js:         'app/js/*.js',
+    js:         'src/js/**/*.js',
     svg:        'src/media-files/img/*.svg',
     img:        'src/media-files/img/**/*.+(jpg|png)'
   },
@@ -52,7 +57,9 @@ const path = {
 
   }
 };
+
 //============================================== fileInclude ====================================================
+
 const fileInclude = () => {
   return gulp
     .src(path.src.html)
@@ -64,21 +71,23 @@ const fileInclude = () => {
       })
     )
     .pipe(gulp.dest(path.build.html))
-    .pipe(browserSync.stream());
-} 
+    .pipe(browserSync.stream())
+} ;
+
 //============================================== SASS ====================================================
+
 const styles = () => {
   return gulp
     .src(path.src.sass)
     .pipe(plumber())
     .pipe(gulpif(isDev, sourcemaps.init()))
-    .pipe(sass(/* {includePaths: require('node-normalize-scss').includePaths} */))
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 2 versions'],
+    .pipe(sass())
+    .pipe(gulpif(isProd, autoprefixer({
+      overrideBrowserslist: ['last 5 versions'],
       cascade: false
-    }))
+    })))
     .pipe(gcmq())
-    .pipe(cleanCSS({
+    .pipe(gulpif(isProd, cleanCSS({
         level: {
           1: {
             all: true,
@@ -88,24 +97,52 @@ const styles = () => {
             restructureRules: true
           }
         }
-    }))
+    })))
     .pipe(rename({suffix: ".min"}))
-    .pipe(sourcemaps.write()) 
+    .pipe(gulpif(isDev, sourcemaps.write())) 
     .pipe(gulp.dest(path.build.css))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.stream())
 };
+
 //============================================== SCRIPTS ====================================================
 
+const jsVen = () => {
+  return gulp
+  .src(path.src.jsVen)
+  .pipe(uglify())
+  .pipe(rename({suffix: ".min"}))
+  .pipe(gulp.dest(path.build.jsVen))
+  
+};
 
+const js = () => {
+  return gulp
+  .src(path.src.js)
+  .pipe(gulpif(isDev, sourcemaps.init()))
+  .pipe(babel())
+  .pipe(jshint())
+  .pipe(jshint.reporter("default"))
+  .pipe(uglify())
+  .pipe(rename({suffix: ".min"}))
+  .pipe(gulpif(isDev, sourcemaps.write()))
+  .pipe(gulp.dest(path.build.js))
+  .pipe(browserSync.stream())
+};
+
+const scripts = gulp.parallel(jsVen, js)
+ 
 
 //============================================== clear ====================================================
+
 const clearBuild = () => {
   return del(path.clear.clean);
 };
 const clearImg = () => {
   return del(path.clear.cleanImg);
-};
-//============================================== IMAGES ====================================================
+}; 
+
+//============================================== SVG ====================================================
+
 const svg = () => {
   return gulp.src(path.src.svg)
   .pipe(svgmin({
@@ -128,10 +165,11 @@ const svg = () => {
     svg: {
       symbols:'svg-sprite.html'
     }
-  }
-  ))
-    .pipe(gulp.dest(path.build.svg));
-}
+  }))
+    .pipe(gulp.dest(path.build.svg))
+};
+
+//============================================== IMAGES ====================================================
 
 const image = () => {
   return gulp
@@ -140,7 +178,7 @@ const image = () => {
       .pipe(rename(function(path) {
         path.dirname = ''
       }))
-      .pipe(gulp.dest(path.build.img));
+      .pipe(gulp.dest(path.build.img))
 };
 //============================================== watcher ====================================================
 const watch = () => {
@@ -154,11 +192,13 @@ const watch = () => {
   gulp.watch(path.watch.html, fileInclude);
   gulp.watch(path.watch.svg, svg);
   gulp.watch(path.watch.img, gulp.series(clearImg, image));
+  gulp.watch(path.watch.js, js);
 };
 
 //============================================== TASK ====================================================
 const build = gulp.series(clearBuild, 
-  gulp.parallel(styles, svg, image, fileInclude));
+  gulp.parallel(scripts, styles, svg, image, fileInclude)
+);
 
 gulp.task('default', gulp.series(build, watch));
 
